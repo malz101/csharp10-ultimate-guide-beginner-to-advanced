18aa63bd520fcfc19824e6e6f6f01b3b9d8c6341
Malik Edwards <malikedwards777@gmail.com>
2023-02-16 00:15:55 -0500

    assignment 1 and assignment 2

---
+++ 01. C# Language Basics/Assignment 1/AreaOfCircle/AreaOfCircle/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaOfCircle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //variable declarations
+            double radius;
+            double pi = 3.14159;
+            double area;
+
+            Console.WriteLine("*Area of Circle Calculator*");
+            Console.WriteLine();
+            Console.Write("Enter radius of circle: ");
+            radius = Convert.ToDouble(Console.ReadLine());
+            
+            Console.WriteLine();
+
+            area = pi * radius * radius;
+
+            Console.WriteLine($"Area of circle is {area}");
+            
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 01. C# Language Basics/Assignment 2/FeetToCentimeters/FeetToCentimeters/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FeetToCentimeters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //variable declarations
+            double feet;
+            double inches;
+            double centimeters;
+
+            //print program name
+            Console.WriteLine("*Feet to Centimeters*");
+
+            //read feet and inches values
+            Console.WriteLine("Enter height");
+            Console.Write("Feet: ");
+            feet = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Inches: ");
+            inches = Convert.ToDouble(Console.ReadLine());
+
+            //convert height to centimeters
+            centimeters = (feet * 12 + inches) * 2.54;
+            Console.WriteLine($"Height in centimeters is {centimeters}");
+
+            Console.ReadKey();
+        }
+    }
+}
